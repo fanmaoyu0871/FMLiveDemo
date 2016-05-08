fdca6d1ea09404f3511862a6addc2f589f0aa842
fanmaoyu0871 <frank0871@163.com>
2016-05-08 12:39:11 +0800

    Initial Commit

---
+++ MeiguangLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeiguangLive
+//
+//  Created by 范茂羽 on 16/5/8.
+//  Copyright © 2016年 范茂羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MeiguangLive/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MeiguangLive
+//
+//  Created by 范茂羽 on 16/5/8.
+//  Copyright © 2016年 范茂羽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
